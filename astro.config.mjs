0fa66e6cdbc15768bba1e843504d0c3dcfa5c864
tenutaletrerose <tenutaletrerose@gmail.com>
2024-05-29 22:15:49 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), icon()],
+  integrations: [tailwind(), preact(), icon(), sitemap()],
   output: "server",
   adapter: netlify(),
-  site: "https://darling-trifle-c84866.netlify.app"
+  site: "https://darling-trifle-c84866.netlify.app",
+  
 });
