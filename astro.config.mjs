73f05a016df0f4a3337792c7bc845bf29c573e05
tenutaletrerose <tenutaletrerose@gmail.com>
2024-06-01 18:21:08 +0200

    compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), icon(), sitemap()],
+  integrations: [tailwind(), preact(), icon(), sitemap(), (await import("@playform/compress")).default()],
   output: "server",
   adapter: netlify(),
-  site: "https://darling-trifle-c84866.netlify.app",
+  site: "https://darling-trifle-c84866.netlify.app"
 });
